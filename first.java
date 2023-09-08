7c7bdb9b3239840e2fc1af8b7ef077502ba7402f
Harish Ravi <39822028+harishravi121@users.noreply.github.com>
2023-09-08 12:18:00 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,7 @@
+// Harish's first java programme on eclipse Hope to code a lot more
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
